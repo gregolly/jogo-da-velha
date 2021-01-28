8f2b9b199a6c6e53ccb4907dda661a19e225a482
Gregolly frança <42480355+gregolly@users.noreply.github.com>
2021-01-27 22:41:51 -0400

    Criado o primiero object.

---
+++ src/objects/Sobre/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+function Sobre() {
+  return (
+    <div>
+      <a href="#to-do" className="sobre">Sobre</a>
+    </div>
+  )
+}
+
+export default Sobre
