e2696039a38249e81b977e6726f7ae683e89d4ad
Gregolly frança <42480355+gregolly@users.noreply.github.com>
2021-02-15 21:47:41 -0400

    Criamos component Header.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import Logo from './objects/Logo'
-import Sobre from './objects/Sobre'
-import Menu from './objects/Menu'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>
-      <Logo />
-      <Sobre />
-      <Menu />
-    </div>
+    <Header />
   )
 }
 
